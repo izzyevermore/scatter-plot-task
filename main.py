9fb8892adf6eaa9ee221026c49678c471dc6eea6
izzyevermore <isaiahlekay2001@gmail.com>
2020-11-06 14:14:04 +0200

    new changes

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
+from matplotlib import *
 import numpy as np
 
 x = np.array([14.2, 16.5, 11.8, 15.3, 18.8, 22.5, 19.5])
 y = np.array([220, 330, 190, 340, 410, 445, 415])
 
-plt.scatter(x, y, color="black", zorder=1)
-plt.plot(x, y, color="green", zorder=2)
+m, b = np.polyfit(x, y, 1)
+plt.scatter(x, y, color="black")
+plt.plot(x, m*x + b)
+plt.plot(x, y, 'o')
 
 plt.xlabel("Temperature")
 plt.ylabel("Price in (R)")
